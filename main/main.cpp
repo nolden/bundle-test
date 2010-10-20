d68c6dc7131773edc07e5a46cc4bd21bc275edd2
Markus Engel <engelm@mbi070.inet.dkfz-heidelberg.de>
2010-10-20 16:57:31 +0200

    Working version with libA as plugin

--- main/main.cpp
+++ main/main.cpp
@@ -1,10 +1,15 @@
 // #include <libA.h>
 // #include <libB.h>
+#include <dlfcn.h>
+#include <iostream>
 
 int main(int , char**)
 {
   // A a;
   // B b;
+  void * p = dlopen("/Users/engelm/bundle-test/install/plugins/core/liblibA.dylib",RTLD_NOW);
+  if (p) { std::cout << "p yes" ; }
+
   return 0;
 
 }
